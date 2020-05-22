328c8800f5f200b7f158e205ff60e495b28e514f
Alex Lopes <alexlr10@hotmail.com>
2020-05-22 15:46:18 -0300

    datatables em portugues

--- legacy/settings.py
+++ legacy/settings.py
@@ -65,21 +65,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'legacy.urls'
 
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': ['templates'],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'legacy.wsgi.application'
 
@@ -98,16 +98,16 @@ DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dbur
 #     }
 # }
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gestao_interna',
-        'USER': 'postgres',
-        'PASSWORD': '91851007',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gestao_interna',
+#         'USER': 'postgres',
+#         'PASSWORD': '91851007',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 #
 # DATABASES = {
 #     'default': {
