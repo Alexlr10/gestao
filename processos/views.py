fee6e695efde9d3fcb35c7c6d0e8b395312c8502
Alex Lopes <alexlr10@hotmail.com>
2020-05-30 16:20:36 -0300

    descomentando salvamento do form

--- processos/views.py
+++ processos/views.py
@@ -218,7 +218,7 @@ def aviso(request):
         form = AvisoForm(request.POST)
 
         if form.is_valid():
-           # form.save()
+            form.save()
             Data = request.POST.get('Data')
             assunto = request.POST.get('assunto')
             data = Data.split('-')
