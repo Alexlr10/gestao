6abb459eb083e0c8135f275e90cb0b16514791a2
Alex Lopes <alexlr10@hotmail.com>
2020-06-03 16:25:22 -0300

    retirando modulo djangoselect2 de apps

--- legacy/settings.py
+++ legacy/settings.py
@@ -42,7 +42,6 @@ AUTH_USER_MODEL = 'processos.Usuario'
 INSTALLED_APPS = [
     'dal',
     'dal_select2',
-    'django_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
